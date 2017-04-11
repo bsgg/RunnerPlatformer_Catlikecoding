cac13de026cc40991caebf038804b482627a51ea
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2017-04-11 17:19:02 +0100

    Added physics for main player

---
+++ RunnerPlatformer/Assets/RunnerPlatformer/Scripts/PlatformerManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunnerPlatformer
+{
+    public class PlatformerManager : MonoBehaviour
+    {
+
+        [Header("Queue Settings")]
+        [SerializeField]
+        private Transform m_Prefab;
+
+        [SerializeField]
+        private int m_NumberOfObjects;
+
+        [SerializeField]
+        private float m_RecycleOffset;
+        [SerializeField]
+        private Vector3 m_StartPosition;
+
+        private Vector3 m_NextPosition;
+
+        private Queue<Transform> m_ObjectQueue;
+
+        [Header("Platform Settings")]
+        [SerializeField]
+        private Vector3 m_MinSize, m_MaxSize;
+
+        [SerializeField]
+        private Vector3 m_MinGap, m_MaxGap;
+
+        [SerializeField]
+        private float m_MinY, m_MaxY;
+
+        void Start()
+        {
+            m_ObjectQueue = new Queue<Transform>(m_NumberOfObjects);
+
+            for (int i = 0; i < m_NumberOfObjects; i++)
+            {
+                m_ObjectQueue.Enqueue((Transform)Instantiate(m_Prefab));
+            }
+
+            m_NextPosition = m_StartPosition;
+
+            // Instance objects
+            for (int i = 0; i < m_NumberOfObjects; i++)
+            {
+                Recycle();
+            }
+        }
+
+        private void Recycle()
+        {
+            Vector3 scale = new Vector3(
+            Random.Range(m_MinSize.x, m_MaxSize.x),
+            Random.Range(m_MinSize.y, m_MaxSize.y),
+            Random.Range(m_MinSize.z, m_MaxSize.z));
+
+            Vector3 position = m_NextPosition;
+            position.x += scale.x * 0.5f;
+            position.y += scale.y * 0.5f;
+
+            Transform o = m_ObjectQueue.Dequeue();
+            o.localScale = scale;
+            o.localPosition = position;
+            m_ObjectQueue.Enqueue(o);
+
+            m_NextPosition += new Vector3(
+                    Random.Range(m_MinGap.x, m_MaxGap.x + scale.x),
+                    Random.Range(m_MinGap.y, m_MaxGap.y),
+                    Random.Range(m_MinGap.z, m_MaxGap.z));
+
+            if (m_NextPosition.y < m_MinY)
+            {
+                m_NextPosition.y = m_MinY + m_MaxGap.y;
+            }else if (m_NextPosition.y > m_MaxY)
+            {
+                m_NextPosition.y = m_MaxY - m_MaxGap.y;
+            }            
+        }
+
+        void Update()
+        {
+            // Change teh position
+            if (m_ObjectQueue.Peek().localPosition.x + m_RecycleOffset < Runner.m_DistanceTraveled)
+            {
+                Recycle();
+            }
+        }
+
+    }
+}
--- RunnerPlatformer/Assets/RunnerPlatformer/Scripts/Runner.cs
+++ RunnerPlatformer/Assets/RunnerPlatformer/Scripts/Runner.cs
@@ -9,10 +9,51 @@ namespace RunnerPlatformer
 
         public static float m_DistanceTraveled;
 
+        [SerializeField]
+        private float m_Acceleration;
+
+        private Rigidbody m_RigidBodyComponent;
+        
+        private bool m_TouchingPlatform;
+
+        [SerializeField]
+        private Vector3 m_JumpVelocity;
+
+        private void Start()
+        {
+            m_RigidBodyComponent = GetComponent<Rigidbody>();
+        }
+
+
         void Update()
         {
-            transform.Translate(5f * Time.deltaTime, 0f, 0f);
+            //transform.Translate(5f * Time.deltaTime, 0f, 0f);
+
+            if (m_TouchingPlatform && Input.GetButtonDown("Jump"))
+            {
+                m_RigidBodyComponent.AddForce(m_JumpVelocity, ForceMode.VelocityChange);
+                m_TouchingPlatform = false;
+            }
+
             m_DistanceTraveled = transform.localPosition.x;
+        }
+
+        private void FixedUpdate()
+        {
+            if (m_TouchingPlatform)
+            {
+                m_RigidBodyComponent.AddForce(m_Acceleration, 0.0f, 0.0f, ForceMode.Acceleration);
+            }
+        }
+
+        void OnCollisionEnter()
+        {
+            m_TouchingPlatform = true;
+        }
+
+        void OnCollisionExit()
+        {
+            m_TouchingPlatform = false;
         }
 
 
